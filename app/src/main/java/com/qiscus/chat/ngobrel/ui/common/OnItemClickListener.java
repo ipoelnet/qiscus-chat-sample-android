cc0a91442197c549cbe743b0dedbcc61dda8d077
Zetra <zetra@mail.ugm.ac.id>
2018-05-16 13:52:06 +0700

    Add on item click listerner

---
+++ app/src/main/java/com/qiscus/chat/ngobrel/ui/common/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.qiscus.chat.ngobrel.ui.common;
+
+/**
+ * Created on : May 16, 2018
+ * Author     : zetbaitsu
+ * Name       : Zetra
+ * GitHub     : https://github.com/zetbaitsu
+ */
+public class OnItemClickListener {
+}
---
+++ app/src/main/java/com/qiscus/chat/ngobrel/ui/common/OnItemLongClickListener.java
@@ -0,0 +1,11 @@
+package com.qiscus.chat.ngobrel.ui.common;
+
+/**
+ * Created on : May 16, 2018
+ * Author     : zetbaitsu
+ * Name       : Zetra
+ * GitHub     : https://github.com/zetbaitsu
+ */
+public interface OnItemClickListener {
+    void onItemClick(int position);
+}
