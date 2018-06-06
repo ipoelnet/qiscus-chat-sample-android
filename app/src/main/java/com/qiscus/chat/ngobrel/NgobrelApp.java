203443f7bb96ecd4b999b01bbac1e011dc186f92
Zetra <zetra@mail.ugm.ac.id>
2018-06-06 13:02:19 +0700

    Add stetho

--- app/src/main/java/com/qiscus/chat/ngobrel/NgobrelApp.java
+++ app/src/main/java/com/qiscus/chat/ngobrel/NgobrelApp.java
@@ -1,10 +1,14 @@
 package com.qiscus.chat.ngobrel;
 
 import android.app.Application;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 
+import com.facebook.stetho.Stetho;
 import com.qiscus.chat.ngobrel.ui.homepagetab.HomePageTabActivity;
 import com.qiscus.chat.ngobrel.util.ChatRoomNavigator;
 import com.qiscus.sdk.Qiscus;
+import com.qiscus.sdk.data.model.QiscusReplyPanelConfig;
 
 /**
  * Created by omayib on 18/09/17.
@@ -24,6 +28,10 @@ public class NgobrelApp extends Application {
         INSTANCE = this;
         component = new AppComponent(this);
 
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this);
+        }
+
         Qiscus.setEnableLog(BuildConfig.DEBUG);
         Qiscus.init(this, "ngobrel-bwk4c0g9fiwzg");
         Qiscus.getChatConfig()
@@ -36,6 +44,9 @@ public class NgobrelApp extends Application {
                 .setReadIconColor(R.color.colorAccent)
                 .setReplyBarColor(R.color.colorPrimary)
                 .setReplySenderColor(R.color.colorPrimary)
+                .setStartReplyInterceptor(comment ->
+                        new QiscusReplyPanelConfig().setBarColor(ContextCompat.getColor(this, R.color.colorPrimary)))
+                .setRoomReplyBarColorInterceptor(qiscusComment -> R.color.colorPrimary)
                 .setEmptyRoomImageResource((R.drawable.ic_room_empty))
                 .setEnableFcmPushNotification(true)
                 .setNotificationBigIcon(R.drawable.ic_logo_qiscus)
